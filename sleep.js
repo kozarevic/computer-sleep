cb3ae94cf59d03566449f595a749362dad5c2bb7
jaruba <branza.alex@gmail.com>
2015-07-25 02:40:14 +0300

    Converted to NPM Module

--- sleep.js
+++ sleep.js
@@ -24,42 +24,42 @@
      sleep.allow()
 
  */
-var sleep = {
-    prevent: function() {
-        if (!this._video) {
-            this._init();
-        }
 
-        this._video.setAttribute('loop', 'loop');
-        this._video.play();
-    },
-    allow: function() {
-        if (!this._video) {
-            this._init();
-        }
+var path = require('path');
+var relbase = "/"+path.relative(path.dirname(require.main.filename), __dirname);
 
-        this._video.removeAttribute('loop');
-        this._video.pause();
-    },
-    _init: function() {
-        this._video = document.createElement('video');
-        this._video.setAttribute('width', '10');
-        this._video.setAttribute('height', '10');
-        this._video.style.position = 'absolute';
-        this._video.style.top = '-10px';
-        this._video.style.left = '-10px';
+module.exports = {
+  prevent: function() {
+    if (!this._video) this._init();
 
-        var source_mp4 = document.createElement('source');
-        source_mp4.setAttribute('src', 'https://github.com/ivanmaeder/computer-sleep/raw/master/resources/muted-blank.mp4');
-        source_mp4.setAttribute('type', 'video/mp4');
-        this._video.appendChild(source_mp4);
+    this._video.setAttribute('loop', 'loop');
+    this._video.play();
+  },
+  allow: function() {
+    if (!this._video) this._init();
 
-        var source_ogg = document.createElement('source');
-        source_ogg.setAttribute('src', 'https://github.com/ivanmaeder/computer-sleep/raw/master/resources/muted-blank.ogv');
-        source_ogg.setAttribute('type', 'video/ogg');
-        this._video.appendChild(source_ogg);
+    this._video.removeAttribute('loop');
+    this._video.pause();
+  },
+  _init: function() {
+    this._video = window.document.createElement('video');
+    this._video.setAttribute('width', '10');
+    this._video.setAttribute('height', '10');
+    this._video.style.position = 'absolute';
+    this._video.style.top = '-10px';
+    this._video.style.left = '-10px';
 
-        document.body.appendChild(this._video);
-    },
-    _video: null
-}
+    var source_mp4 = window.document.createElement('source');
+    source_mp4.setAttribute('src', relbase+'/resources/muted-blank.mp4');
+    source_mp4.setAttribute('type', 'video/mp4');
+    this._video.appendChild(source_mp4);
+
+    var source_ogg = window.document.createElement('source');
+    source_ogg.setAttribute('src', relbase+'/resources/muted-blank.ogv');
+    source_ogg.setAttribute('type', 'video/ogg');
+    this._video.appendChild(source_ogg);
+
+    window.document.body.appendChild(this._video);
+  },
+  _video: null
+};
